bc99907de62385e89c911fb3c70592f1286076a2
YuakaTL <B10823036@yuntech.edu.tw>
2023-05-02 02:05:16 +0800

    fix: 🐛 ESLint error Missing "key" prop for element in array

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -35,6 +35,7 @@ export default function Home() {
           centered={true}
           footer={[
             <CustomButton
+              key="submit"
               text="確認"
               className="w-full bg-purple"
               onClick={handleOk}
