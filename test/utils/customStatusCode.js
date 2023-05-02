03c99d281371fd37eb5b0a7ec8ba52977230a54a
VictorChao996 <96094victor@gmail.com>
2023-05-02 16:34:43 +0800

    test: update utils folder

--- test/utils/customStatusCode.js
+++ test/utils/customStatusCode.js
@@ -6,6 +6,7 @@ export default {
     StartTimeIncorrectError: "003", //開始時間需大於設定當日
     lessThan10MinModifyError: "004", //活動開始前10分鐘後除隱藏外不可修改任何欄位
     lotteryDrawExceed30SecError: "005", //抽獎獎品超過30秒未領取
+    memberNoDiscountError: "006", //此會員沒有可用的折扣卷
     inputValueInvalidError: "100", //params 或 request body 輸入值不符合規定(type錯誤)
     accessTokenError: "101", //Authorization header 沒有帶 access_token(或錯誤)
     unknownError: "999",
