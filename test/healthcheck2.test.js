5c1bbf13fa9cf71714caa27c5cad39351ea1acce
VictorChao996 <96094victor@gmail.com>
2023-05-02 11:55:09 +0800

    fix: healthCheck.test.js

--- test/healthcheck2.test.js
+++ test/healthcheck2.test.js
@@ -5,7 +5,6 @@ describe(`healthcheck`, () => {
     it("should return 200 OK status", async () => {
         const res = await require(app).get("/api/v1/healthcheck").expect(200);
 
-        // expect(res.body).toHaveProperty("status", "ok");
-        expect(res.body).toHaveProperty("status");
+        expect(res.body).toHaveProperty("status", "ok");
     });
 });
