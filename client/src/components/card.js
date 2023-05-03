1bd15b92b71adc91ad1a23898cdca0eb3b684488
YuakaTL <B10823036@yuntech.edu.tw>
2023-05-03 23:38:59 +0800

    refactor: 💡 Use convertDiscount for discount bg colors

--- client/src/components/card.js
+++ client/src/components/card.js
@@ -1,5 +1,4 @@
 export default function Card({
-  className,
   discount,
   title,
   description,
@@ -8,11 +7,18 @@ export default function Card({
   if (textAlign === "left") {
     textAlign = "text-left ml-[22px] mr-auto";
   }
+  const convertDiscount = {
+    1: "bg-yellow",
+    5: "bg-blue",
+    8: "bg-green",
+    9: "bg-red",
+    undefined: "bg-gray",
+  };
 
   return (
     <div className="m-4 flex items-center justify-between  rounded-[20px] bg-white p-4 shadow-2">
       <div
-        className={`${className} flex h-20 w-20 items-center justify-center rounded-[10px] text-2xl font-black text-white`}
+        className={`${convertDiscount[discount]} flex h-20 w-20 items-center justify-center rounded-[10px] text-2xl font-black text-white`}
       >
         {discount && `${discount} 折`}
       </div>
