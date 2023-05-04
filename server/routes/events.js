eeb4ba029824eb750ccde6e65f25c40200cfa0a7
egnkvn <b09902021@ntu.edu.tw>
2023-05-05 00:39:46 +0800

    feat: 🎸 add PUT lottery/receive api

--- server/routes/events.js
+++ server/routes/events.js
@@ -5,11 +5,11 @@ import eventsController from "../controllers/eventsController.js";
 import isAuth from "../middleware/isAuth.js";
 
 
-const { getEventDetail, updateInventory, createInfo } = eventsController;
+const { getEventDetail, updateInventory, createInfo, updateInfo } = eventsController;
 
 router.get("/event", isAuth, handleErrorAsync(getEventDetail));
 router.put("/inventory/:discount_id", isAuth, handleErrorAsync(updateInventory));
 router.post("/info", isAuth, handleErrorAsync(createInfo));
-
+router.put("/receive/:lottery_id", isAuth, handleErrorAsync(updateInfo));
 
 export default router;
