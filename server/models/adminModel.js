988d45cbd5088feaad5c3ae1305c0918678e3e6f
Brandon P.C. Yeh <57129342+brandon9912@users.noreply.github.com>
2023-05-05 22:16:59 +0800

    style: 💄 improve formatting

--- server/models/adminModel.js
+++ server/models/adminModel.js
@@ -4,24 +4,22 @@ const prisma = new PrismaClient();
 
 const adminModel = {
   getLottery: async (page, pageSize) => {
-    if (page === 999) {
-        const result = await prisma.event.findMany({
-                include: {
-                    Discount: true,
-                },
-            }
-        );
-        return result;
-    }
-    else {
-        const result = await prisma.event.findMany({
-            skip: (page - 1) * pageSize,
-            take: pageSize + 1,
-            include: {
-                Discount: true,
-            },
-        });
-        return result;
+    if (pageSize === 999) {
+      const result = await prisma.event.findMany({
+        include: {
+          Discount: true,
+        },
+      });
+      return result;
+    } else {
+      const result = await prisma.event.findMany({
+        skip: (page - 1) * pageSize,
+        take: pageSize + 1,
+        include: {
+          Discount: true,
+        },
+      });
+      return result;
     }
   },
 };
