90d415e9a2506bb1542b0b52cddae5b8b988948d
Brandon P.C. Yeh <57129342+brandon9912@users.noreply.github.com>
2023-05-05 22:12:28 +0800

    refactor: 💡 remove other functionalities(create lottery)

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -4,9 +4,8 @@ import handleErrorAsync from "../utils/handleErrorAsync.js";
 import adminController from "../controllers/adminController.js";
 import isAuth from "../middleware/isAuth.js";
 
-const { createLottery, getLottery } = adminController;
+const { getLottery } = adminController;
 
-router.post("/lottery", handleErrorAsync(createLottery));
 router.get("/lottery", handleErrorAsync(getLottery));
 
 export default router;
