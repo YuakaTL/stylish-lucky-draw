8b63b0e3a1a53d0a1e46d1eae9eb786acfc99a60
YuakaTL <B10823036@yuntech.edu.tw>
2023-05-07 16:22:53 +0800

    feat: 🎸 custom button components add loading effect

--- client/src/components/button.js
+++ client/src/components/button.js
@@ -1,10 +1,26 @@
-export default function Button({ text, className, onClick }) {
+import { Spin } from "antd";
+import { LoadingOutlined } from "@ant-design/icons";
+
+const antIcon = (
+  <LoadingOutlined
+    style={{
+      fontSize: 20,
+      color: "white",
+    }}
+    spin
+  />
+);
+
+export default function Button({ text, className, onClick, isLoading }) {
   return (
     <button
       type="button"
-      className={`${className} h-12 rounded-[10px] text-white  shadow hover:opacity-80`}
+      className={`${className} ${
+        isLoading && "cursor-not-allowed !bg-gray-500"
+      } flex h-12 items-center  justify-center gap-5 rounded-[10px] text-white shadow hover:opacity-80`}
       onClick={onClick}
     >
+      {isLoading && <Spin indicator={antIcon} />}
       {text}
     </button>
   );
