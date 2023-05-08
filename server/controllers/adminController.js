4355cb2734cba85a19063048c35e33ffecf79047
michelle <michelle190625@gmail.com>
2023-05-08 15:27:44 +0800

    feat: 🎸 complete lottery put api

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -2,8 +2,70 @@ import { adminModel } from "../models/adminModel.js";
 import successHandle from "../utils/successHandle.js";
 import appError from "../utils/appError.js";
 import validator from "../utils/validation.js";
+import xlsx from "xlsx";
 
 const adminController = {
+  updateLottery: async (req, res, next) => {
+    const { event_name, event_start_time, event_end_time, is_visible, status } =
+      req.body;
+
+    // exist check
+    validator.existValidate(req.params.event_id, "event_id", next);
+    validator.existValidate(event_name, "event_name", next);
+    validator.existValidate(event_start_time, "event_start_time", next);
+    validator.existValidate(event_end_time, "event_end_time", next);
+    validator.existValidate(is_visible, "is_visible", next);
+    validator.existValidate(status, "status", next);
+
+    // handle time format
+    validator.endStartValidate(event_start_time, event_end_time, next);
+    validator.nowStartValidate(event_start_time, next);
+    validator.tenMinutesValidate(event_start_time, next);
+
+    // turn string into boolean
+    const is_visible_bool = is_visible === "true";
+
+    // handle excel data
+    validator.existValidate(req.file, "req.file", next);
+    const filePath = req.file.path;
+    const file = xlsx.readFile(filePath);
+    const sheets = file.SheetNames;
+    let data = [];
+
+    for (let i = 0; i < sheets.length; i++) {
+      const temp = xlsx.utils.sheet_to_json(file.Sheets[file.SheetNames[i]]);
+      temp.forEach((res) => {
+        data.push(res);
+      });
+    }
+
+    const result = await adminModel.updateLottery(
+      req.params.event_id,
+      event_name,
+      event_start_time,
+      event_end_time,
+      is_visible_bool,
+      status,
+      data,
+      next
+    );
+
+    // return
+    const lottery_data = {
+      event_id: result.event_id,
+      event_name: result.event_name,
+      event_start_time: result.event_start_time,
+      event_end_time: result.event_end_time,
+      is_visible: result.is_visible,
+      status: result.status,
+      event_data: result.event_data,
+      create_at: result.create_time,
+      update_at: result.last_update_time,
+    };
+
+    successHandle(res, "更新成功", lottery_data);
+  },
+
   getLottery: async (req, res, next) => {
     const { paging, amount } = req.query;
 
--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,11 +1,46 @@
 import { Router } from "express";
-const router = Router();
 import handleErrorAsync from "../utils/handleErrorAsync.js";
 import adminController from "../controllers/adminController.js";
-import isAuth from "../middleware/isAuth.js";
+import multer from "multer";
+import path from "path";
+import fs from "fs";
+const router = Router();
 
-const { getLottery } = adminController;
+// Set storage engine of multer
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: (req, file, cb) => {
+      const uploadDir = "./uploads";
+      if (!fs.existsSync(uploadDir)) {
+        fs.mkdirSync(uploadDir);
+      }
+      cb(null, uploadDir);
+    },
+    filename: (req, file, cb) => {
+      cb(null, file.originalname);
+    },
+  }),
+  fileFilter: (req, file, cb) => {
+    const filetypes = /xlsx|xls|XLSX|XLS/;
+
+    const extname = filetypes.test(
+      path.extname(file.originalname).toLowerCase()
+    );
+
+    if (extname) {
+      return cb(null, true);
+    }
+    cb(new Error("The file must be an Excel file!"));
+  },
+});
+
+const { getLottery, updateLottery } = adminController;
 
 router.get("/lottery", handleErrorAsync(getLottery));
+router.put(
+  "/lottery/:event_id",
+  upload.single("excel_file"),
+  handleErrorAsync(updateLottery)
+);
 
 export default router;
