f7661a82935e4aa202a788c845bc05225da03d78
YuakaTL <B10823036@yuntech.edu.tw>
2023-05-09 18:15:30 +0800

    feat: 🎸 Integrate Event page delete lottery API

--- client/src/pages/admin/event.js
+++ client/src/pages/admin/event.js
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import { useState, useEffect } from "react";
-import { Table, Tag } from "antd";
+import { Table, Tag, message } from "antd";
 import { EditOutlined, DeleteTwoTone } from "@ant-design/icons";
 import dayjs from "dayjs";
 import axios from "axios";
@@ -20,126 +20,10 @@ const convertStatusColor = {
   finished: "red",
 };
 
-const editEvent = (data) => () => {
-  console.log("editEvent", data);
-};
-
-const deleteEvent = (id) => async () => {
-  let result = await axios.put(
-    `${process.env.NEXT_PUBLIC_API_URL}/api/v1/admin/lottery/${id}`,
-    {
-      status: "cancelled",
-    }
-  );
-};
-
-const columns = [
-  {
-    title: "活動序號",
-    dataIndex: "event_id",
-    width: "10%",
-    sorter: {
-      compare: (a, b) => a.event_id - b.event_id,
-    },
-    sortDirections: ["descend"],
-  },
-  {
-    title: "活動名稱",
-    dataIndex: "event_name",
-    width: "15%",
-  },
-  {
-    title: "活動開始時間",
-    dataIndex: "event_start_time",
-    sorter: true,
-    width: "15%",
-    render: (event_start_time) => (
-      <>{dayjs(event_start_time).format("YYYY-MM-DD HH:mm:ss")}</>
-    ),
-    sorter: {
-      compare: (a, b) =>
-        dayjs(a.event_end_time).unix() - dayjs(b.event_end_time).unix(),
-    },
-  },
-  {
-    title: "活動結束時間",
-    dataIndex: "event_end_time",
-    sorter: true,
-    width: "15%",
-    render: (event_end_time) => (
-      <>{dayjs(event_end_time).format("YYYY-MM-DD HH:mm:ss")}</>
-    ),
-    sorter: {
-      compare: (a, b) =>
-        dayjs(a.event_end_time).unix() - dayjs(b.event_end_time).unix(),
-    },
-  },
-  {
-    title: "狀態",
-    dataIndex: "status",
-    width: "15%",
-    render: (status, data) => (
-      <>
-        {data.is_visible ? (
-          <Tag color={convertStatusColor[status]} key={status}>
-            {convertStatus[status]}
-          </Tag>
-        ) : (
-          <Tag color={"red"} key={status}>
-            已隱藏
-          </Tag>
-        )}
-      </>
-    ),
-    filters: [
-      {
-        text: "尚未開始",
-        value: "pending",
-      },
-      {
-        text: "進行中",
-        value: "ongoing",
-      },
-      {
-        text: "已取消",
-        value: "cancelled",
-      },
-      {
-        text: "已結束",
-        value: "finished",
-      },
-    ],
-    onFilter: (value, record) => record.status.indexOf(value) === 0,
-  },
-  {
-    title: "獎品剩餘庫存",
-    dataIndex: "total_inventory",
-    width: "15%",
-  },
-  {
-    title: "功能",
-    dataIndex: "event_id",
-    align: "center",
-    width: "10%",
-    render: (id, data) => (
-      <div className="flex justify-around">
-        <EditOutlined
-          className="cursor-pointer text-xl"
-          onClick={editEvent(data)}
-        />
-        <DeleteTwoTone
-          twoToneColor="#A30D11"
-          className="cursor-pointer text-xl"
-          onClick={deleteEvent(id)}
-        />
-      </div>
-    ),
-  },
-];
-
 export default function Event() {
   const [data, setData] = useState();
   const [loading, setLoading] = useState(false);
+  const [messageApi, contextHolder] = message.useMessage();
   const [tableParams, setTableParams] = useState({
     pagination: {
       current: 1,
@@ -158,9 +42,127 @@ export default function Event() {
     },
   });
 
+  const columns = [
+    {
+      title: "活動序號",
+      dataIndex: "event_id",
+      width: "10%",
+      sorter: {
+        compare: (a, b) => a.event_id - b.event_id,
+      },
+      sortDirections: ["descend"],
+    },
+    {
+      title: "活動名稱",
+      dataIndex: "event_name",
+      width: "15%",
+    },
+    {
+      title: "活動開始時間",
+      dataIndex: "event_start_time",
+      sorter: true,
+      width: "15%",
+      render: (event_start_time) => (
+        <>{dayjs(event_start_time).format("YYYY-MM-DD HH:mm:ss")}</>
+      ),
+      sorter: {
+        compare: (a, b) =>
+          dayjs(a.event_end_time).unix() - dayjs(b.event_end_time).unix(),
+      },
+    },
+    {
+      title: "活動結束時間",
+      dataIndex: "event_end_time",
+      sorter: true,
+      width: "15%",
+      render: (event_end_time) => (
+        <>{dayjs(event_end_time).format("YYYY-MM-DD HH:mm:ss")}</>
+      ),
+      sorter: {
+        compare: (a, b) =>
+          dayjs(a.event_end_time).unix() - dayjs(b.event_end_time).unix(),
+      },
+    },
+    {
+      title: "狀態",
+      dataIndex: "status",
+      width: "15%",
+      render: (status, data) => (
+        <>
+          {data.is_visible ? (
+            <Tag color={convertStatusColor[status]} key={status}>
+              {convertStatus[status]}
+            </Tag>
+          ) : (
+            <Tag color={"red"} key={status}>
+              已隱藏
+            </Tag>
+          )}
+        </>
+      ),
+      filters: [
+        {
+          text: "尚未開始",
+          value: "pending",
+        },
+        {
+          text: "進行中",
+          value: "ongoing",
+        },
+        {
+          text: "已取消",
+          value: "cancelled",
+        },
+        {
+          text: "已結束",
+          value: "finished",
+        },
+      ],
+      onFilter: (value, record) => record.status.indexOf(value) === 0,
+    },
+    {
+      title: "獎品剩餘庫存",
+      dataIndex: "total_inventory",
+      width: "15%",
+    },
+    {
+      title: "功能",
+      dataIndex: "event_id",
+      align: "center",
+      width: "10%",
+      render: (id, data) => (
+        <div className="flex justify-around">
+          <EditOutlined
+            className="cursor-pointer text-xl"
+            onClick={editEvent(data)}
+          />
+          <DeleteTwoTone
+            twoToneColor="#A30D11"
+            className="cursor-pointer text-xl"
+            onClick={deleteEvent(id, data)}
+          />
+        </div>
+      ),
+    },
+  ];
+
+  const editEvent = (data) => () => {
+    console.log("editEvent", data);
+  };
+
+  const deleteEvent = (id, data) => async () => {
+    let result = await axios.delete(
+      `${process.env.NEXT_PUBLIC_API_URL}/api/v1/admin/lottery/${id}`
+    );
+    if (result.data.code === "000") {
+      messageApi.success("刪除成功");
+      fetchData();
+    }
+  };
+
   const fetchData = () => {
     setLoading(true);
-    fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/admin/lottery`)
+    fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/admin/lottery?amount=999`)
       .then((res) => res.json())
       .then((results) => {
         let data = [];
@@ -212,11 +214,8 @@ export default function Event() {
       <h2 className="mb-6 mt-6 border-b border-[#AAAAAA] pb-[6px] text-xl">
         活動管理
       </h2>
-      <Link href={"event-creation"} className="inline-block float-right mb-4">
-        <CustomButton
-          text="+ 新活動"
-          className=" w-28 bg-black text-white"
-        />
+      <Link href={"event-creation"} className="float-right mb-4 inline-block">
+        <CustomButton text="+ 新活動" className=" w-28 bg-black text-white" />
       </Link>
 
       <Table
@@ -229,6 +228,7 @@ export default function Event() {
         responsive={true}
         className="overflow-y-auto"
       />
+      {contextHolder}
     </div>
   );
 }
